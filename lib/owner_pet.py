05977bdf246d403d0b544af6fbf952fed8394bf7
Joseph Lurvey <joeylurvey@gmail.com>
2023-11-08 19:15:30 -0500

    Completed lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,31 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+            return [pet for pet in Pet.all if pet.owner == self] 
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise ValueError("The provided object is not a valid Pet instance")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name.lower())
+
+class Pet:
+    all = []
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    def __init__(self, name, pet_type, owner=''):
+        self.name = name
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise ValueError("Invalid pet type.")
+        self.owner = owner
+        Pet.all.append(self)
+    
+
